914ab263d6396586c7956344c745a1f5c531de08
oka314 <61148080+oka314@users.noreply.github.com>
2020-03-22 20:23:19 +0900

    編集機能
    HTTPメゾットを覚え直す

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,9 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.content = params[:content]
+    @post.save
     redirect_to posts_path
   end
 
